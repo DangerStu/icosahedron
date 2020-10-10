cc0201980889b2d69bffb08fa19d449d36aaa758
Stuart Gregg <sgregg@CommonAreas.com>
2020-10-10 15:49:23 -0700

    Asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,34 @@
 const path = require("path");
 
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
 module.exports = {
 	entry: "./src/index.js",
+	devtool: "inline-source-map",
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: "Development",
+		}),
+	],
 	output: {
-		filename: "main.js",
+		filename: "bundle.js",
 		path: path.resolve(__dirname, "dist"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: ["file-loader"],
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: ["file-loader"],
+			},
+		],
 	},
 };
